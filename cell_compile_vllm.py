d2de97f8a56826516e6fe73d94e44515ac2afdc8
ybiran <yahavb@amazon.com>
2025-04-02 18:24:13 +0000

    SD

---
+++ cell_compile_vllm.py
@@ -0,0 +1,42 @@
+from vllm import LLM, SamplingParams
+import yaml
+import os
+import sys
+from huggingface_hub import create_repo,upload_folder,login
+
+hf_token = os.environ['HUGGINGFACE_TOKEN'].strip()
+compiled_model_name=os.environ['COMPILED_MODEL_NAME']
+#os.environ['COMPILED_MODEL_PATH']=compiled_model_name
+os.environ['NEURON_COMPILED_ARTIFACTS']=compiled_model_name
+os.environ['VLLM_NEURON_FRAMEWORK']='neuronx-distributed-inference'
+
+if len(sys.argv) <= 1:
+  print("Error: Please provide a path to a vLLM YAML configuration file.")
+  sys.exit(1)
+
+config_path = sys.argv[1]
+with open(config_path, 'r') as f:
+  model_vllm_config_yaml = f.read()
+
+login(hf_token,add_to_git_credential=True)
+
+def push_compiled_model_to_hf(local_dir,repo_id,commit_message):
+  create_repo(repo_id=repo_id,exist_ok=True,private=False)
+  upload_folder(folder_path=local_dir,path_in_repo="",repo_id=repo_id,commit_message=commit_message)
+
+model_vllm_config = yaml.safe_load(model_vllm_config_yaml)
+llm_model = LLM(**model_vllm_config)
+sampling_params = SamplingParams(top_k=1, temperature=1.0, max_tokens=64)
+prompt = "What is Annapurna Labs?"
+print(f"Running inference with prompt: '{prompt}'")
+outputs = llm_model.generate([prompt], sampling_params)
+for output in outputs:
+  print("Prompt:", output.prompt)
+  print("Generated text:", output.outputs[0].text)
+
+push_compiled_model_to_hf(
+  local_dir=compiled_model_name,
+  repo_id=compiled_model_name,
+  commit_message=f"Add NxD compiled model {compiled_model_name} for vLLM")
+
+print(f"✅  compilation was successful and stored in {compiled_model_name}!")
